3f8a68eb211afe4b2e5a33080daef7564283bf02
Diego Rubin <rubin.diego@gmail.com>
2014-09-02 19:11:15 -0300

    Working on interface.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def in_action?(action_name)
+    controller.action_name == action_name
+  end
+
+  def is_active?(controller_name)
+    controller.controller_name == controller_name
+  end
+
+  def menu_button(title, path, klass, options = {})
+    link_to(title, path, {class: "btn #{klass}"}.merge(options))
+  end
+
+  def set_active(controller_name)
+    'active' if is_active?(controller_name)
+  end
+
 end
+
